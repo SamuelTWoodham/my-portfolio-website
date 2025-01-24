8c27016a1c44edeb72e66c1bf1c0a5f95b334fa0
Woodham, Samuel <samuel.woodham@theverygroup.com>
2025-01-24 14:57:27 +0000

    24-01:kick-off first setup and installation

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// ... other imports
+
+function App() {
+  return (
+    <div> 
+      {/* Your main application content */}
+    </div>
+  );
+}
+
+export default App;
